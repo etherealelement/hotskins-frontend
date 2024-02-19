5ffe0ee2f0f0a321010c73ddfbbe69aca3f50c06
etherealelement <vitalik.safonov@list.ru>
2024-02-19 10:41:24 +0300

    feat: create-main-header

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import "./global.css";
+import {widgetHeaderUi} from '~/widgets/header';
+import { MantineProvider } from '@mantine/core';
+
+
+const {Header} = widgetHeaderUi;
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <Header></Header>
   </React.StrictMode>,
 )
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from 'path';
+
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    build: {
+        outDir: 'build'
+    },
+    server: {
+        port: 3000
+    },
+    resolve: {
+        alias: {
+            '~': path.resolve(__dirname, 'src')
+        }
+    },
+    plugins: [react(), svgr()]
+});
